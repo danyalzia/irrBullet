6a9a410eef4a4c64d152050b3af45a27feb278b7
danyalzia <catofdanyal@yahoo.com>
2014-07-28 17:10:14 +0500

    Remove dependency .d files

--- src/irrBulletCollisionObjectAffector.d
+++
@@ -1,6 +0,0 @@
-irrBulletCollisionObjectAffector.o: \
- src/irrBulletCollisionObjectAffector.cpp \
- include/irrBulletCollisionObjectAffector.h \
- /home/danyalzia/irrlicht/include/irrTypes.h \
- /home/danyalzia/irrlicht/include/IrrCompileConfig.h \
- include/irrBulletCompileConfig.h
